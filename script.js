581ea534a443c8b3d13777e7dbc76cab470522ea
niroj2338-sys <niroj2338@gmail.com>
2026-02-12 21:25:39 +0545

    added about html, CSS, java

---
+++ script.js
@@ -0,0 +1,14 @@
+function moveRandomEl(elm){
+    elm.style.position ="absolute";
+    elm.style.top =Math.floor(Math.random() * 90 + 5) + "%";
+    elm.style.left =Math.floor(Math.random() * 90 + 5) + "%";
+}
+const moveRando = document.querySelector
+("#move-random");
+
+moveRando.addEventListener("mouseenter", function
+ (e) {
+    moveRandomEl(e.target);
+
+});
+
